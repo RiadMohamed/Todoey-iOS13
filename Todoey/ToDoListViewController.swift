90c80443300fdaf04ba39011403df77a71e292b1
Riad Mohamed <riaduk1@gmail.com>
2020-01-12 02:04:38 +0200

    Create an extension for the ToDoListViewController for the Tableview Datasource Methods.
    Create an extension for the ToDoListViewController for the Tableview Delegate Methods.

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
+    var itemArray = ["Find Mike", "Buy Eggs", "Destroy Dragon"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+
+// MARK: - Tableview Datasource Methods
+extension ToDoListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell")!
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+}
+
+// MARK: - Tableview Delegate Methods
+extension ToDoListViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
